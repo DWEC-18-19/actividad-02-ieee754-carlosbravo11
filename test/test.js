707a30a0ecbafed406c6941cfab4f2a55fb8d666
carlosbravo11 <44261206+carlosbravo11@users.noreply.github.com>
2018-11-22 09:27:26 +0100

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,71 +1,190 @@
 'use strict';
+ 
+//var numeroEntero = 0;  // para el numero dado como variable auxiliar para calcular el binario
+//var cadenabinaria = "";  // para poner en número en formato binario
 
-var should = require('chai').should();
-var ieee754 = require('../js/modulo_ieee754.js');
+var// mantisa
 
-describe('Pruebas sencillas', () => {
-  it('Prueba entero', () => {
-    ieee754.ieee754aBinario("123").should.equal("01000010111101100000000000000000");
-  });
-    it('Prueba entero cadena', () => {
-    ieee754.ieee754aBinario(123).should.equal("01000010111101100000000000000000");
-  });
-});
+main();
+ 
 
-describe('Pruebas números pequeños', () => {
-  it('Prueba precisión', () => {
-    ieee754.ieee754aBinario(4.000015).substring(0,23-5).should.equal("01000000100000000000000000011111".substring(0,23-5));
-  });
-    it('Prueba centesimas ', () => {
-    ieee754.ieee754aBinario("0.008544921875").substring(0,23-5).should.equal("00111100000011000000000000000000".substring(0,23-5));
-  });
-    it('Prueba negativo', () => {
-    ieee754.ieee754aBinario("-0.006").substring(0,23-5).should.equal("10111011110001001001101110100110".substring(0,23-5));
-  });  
-     it('Prueba 0', () => {
-    ieee754.ieee754aBinario("0").should.equal("00000000000000000000000000000000");
-  });
-     it('Prueba -0', () => {
-   ieee754.ieee754aBinario("-0").should.equal("10000000000000000000000000000000");
-  });
-       it('Prueba -5.3333282', () => {
-   ieee754.ieee754aBinario("-5.3333282470703125").should.equal("11000000101010101010101010100000");
-  });
-
-});
-
-describe('Pruebas codewars', () => {
-    it('Prueba -666.01', () => {
-    ieee754.ieee754aBinario("-666.01").substring(0,23-5).should.equal("11000100001001101000000010100100".substring(0,23-5));
-  });
-    it('Prueba 1', () => {
-    ieee754.ieee754aBinario(1).substring(0,23-5).should.equal("00111111100000000000000000000000".substring(0,23-5));
-  });
-      it('Prueba 0.0001', () => {
-    ieee754.ieee754aBinario(0.0001).substring(0,23-5).should.equal("00111000110100011011011100010111".substring(0,23-5));
-  });
-       it('Prueba 0.606', () => {
-    hiker.ieee754.ieee754aBinario(0.606).substring(0,23-5).should.equal("00111111000110110010001011010001".substring(0,23-5));
-  });
-        it('Prueba 12345678 ', () => {
-    ieee754.ieee754aBinario(12345678).substring(0,23-5).should.equal("01001011001111000110000101001110".substring(0,23-5));
-  }); 
-
-
-});
+function main() {
+  ieee754aBinario(80);
+}
 
 
 
-describe('Pruebas notación exponencial', () => {
-  it('Exponencial', () => {
-    ieee754.ieee754aBinario(2.861023E-6).should.equal("00110110010000000000000000000000");
-  });
-    it('Número más grande', () => {
-    ieee754.ieee754aBinario(3.4028172E38).should.equal("01111111011111111111111111100000");
-  });
-    it('Número más pequeño', () => {
-    ieee754.ieee754aBinario(1.1754945E-38).should.equal("00000000100000000000000000000001");
-  });
-});
+// separa la parte entera y la decimal si hay
+//function obtenerParteEnteraDecimal(ivalordado)
+ /// le pasamos un numero y devuele un string
 
 
+function ieee754aBinario(ivalordado)      
+{
+
+    var valordado = ivalordado; // valor que recibe de la caja de texto
+    var dnumeropartido = valordado.split(".");
+    var dParteEntera= dnumeropartido[0];
+    var dParteDecimal= dnumeropartido[1];
+
+
+
+ // signo del número dado 
+    var bsigno;
+  	var sValorBinarioExponente="";
+  
+  
+    bsigno = (Math.sign(valordado)===1) ? 0 : 1;
+
+    // ############################################################################################################################
+    // aqui se cargaria para ver la parte de signo si fuera una caja de texto
+    ///document.getElementById('txtsigno').value=  bsigno;
+    //
+    console.log(dParteEntera);
+    console.log(dParteDecimal);
+
+
+    var parteEnteraBinario= Convierte_Binario(dParteEntera);
+  
+   var parteDecimalBinario;
+  
+    //parte decimal con valor sino no calcula en binario la parte decimal
+    if (dnumeropartido[1] > 0) {    
+         parteDecimalBinario = obtenerParteDecimal_ABinario(dParteDecimal);
+    }
+    else {
+      parteDecimalBinario=0;
+    }
+
+   // numero transformado a binario
+   // ################################################################################################################
+///         document.getElementById('verbinario').value="" + parteEnteraBinario + "," + "" + parteDecimalBinario +"";
+
+  
+    sValorBinarioExponente= exponente(parteEnteraBinario  + parteDecimalBinario );
+
+    //#######################################################################################
+    ///document.getElementById('txtexponente').value=sValorBinarioExponente;
+
+   // aqui se usa la varible para establecer mantisa
+     var sValorBinarioMantisa= parteEnteraBinario  + parteDecimalBinario ;
+     sValorBinarioMantisa= sValorBinarioMantisa.substr(1); // quitamos el bit de la izquierda 
+      // cargar valor en matisa campo txtmantisa
+     // ######################################################################################### ###
+   /// document.getElementById('txtmantisa').value=vermantisa(sValorBinarioMantisa);
+
+      sValorBinarioMantisa= bsigno + " - "+  sValorBinarioExponente+  " - "  + vermantisa(sValorBinarioMantisa) ;
+
+    //nos devuele la cadena valor Matinsa
+    alert(sValorBinarioMantisa);
+    return  sValorBinarioMantisa; 
+}
+/**********************************************************************************************
+ * @@function: obtener la cadena 1 y 0 del resto hasta que la parte entera de la división sea 0
+ * @param {numero parte decimal para transformar binario} ivalordado se pasa por valor
+ * @return obtener la cadena de la parte decimal
+ */
+function obtenerParteDecimal_ABinario(ivalordado)
+{
+  
+  var condicionComprueba;
+  var restoAlmacenado;
+  var bsigno;
+  var   cadenabinaria;
+  //  ejemplo, 0.375 a binario
+  // 0.375 * 2 = 0,75   cogemos el 0 pasamos la parte decimal a multiplicar por 2 sucesivamente
+  // 0.75 * 2 = 1.5     cogemos el 1 
+  // 0.5 * 2 = 1,00         cogemos 1  saldria del bucle al ser la parte decimal 0
+    var numeroDecimal= "0." +  ivalordado;
+        numeroDecimal = parseFloat(numeroDecimal); // (casting para poder ser convertido
+   restoAlmacenado=numeroDecimal;
+    do {                  
+        restoAlmacenado = "" + restoAlmacenado * 2 + "";
+
+        var dnumeropartido =  restoAlmacenado.split(".") ;
+        var dParteEntera= dnumeropartido[0];
+        restoAlmacenado= "0." + dnumeropartido[1] + "";    // convertir en decimal ej 50 como 0,5
+        restoAlmacenado=parseFloat(restoAlmacenado);
+        //En esta variable guardamos el resto y se lo concatenamos al STRING ya almacenado.
+        cadenabinaria = "" + dParteEntera +"" +""+ cadenabinaria  + "" ;
+
+            // ejemplo 0,23 produce un  bucle infinito periodo puro  poner máximo 8 interaciones maximo 
+          if (cadenabinaria.length > 8) break ;
+
+    
+      
+          condicionComprueba=parseInt( dnumeropartido[1] );
+      
+    } while (condicionComprueba > 0);
+
+     cadenabinaria= bsigno + " - " + cadenabinaria;
+    return cadenabinaria;
+
+}
+
+ 
+function exponente(sCadenaBinaria)
+{
+     // si por ejemp es 11101 pasa 1,1101 y ocurrencias exponente de 1 son 3 para luego sumar 127
+   var cexponente =sCadenaBinaria.substr(1);
+   var longitudbinario= cexponente.split("1").length-1;
+
+   longitudbinario = 127 +longitudbinario;
+   var sValorBinarioExponente = Convierte_Binario(longitudbinario); // le pasamos valor para que lo convierta en binario
+   // escribir el resultado en la caja de texto de txtexponente
+   return sValorBinarioExponente;
+
+
+}
+
+
+
+  /**********************************************************************************************
+ * @@function: obtener la cadena 1 y 0 del resto hasta que la parte entera de la división sea 0
+ * @param {numero parte entera para transformar binario} iValorInsertado se pasa por valor
+ * @return obtener la cadena binaria de la parte entera
+ */
+function  Convierte_Binario(iValorInsertado) {
+     var  cadenabinaria="";
+     var  restoAlmacenado;
+  var numeroEntero;
+    // parseInt es para que tome la parte entera
+    numeroEntero = parseInt(iValorInsertado);
+     
+            do {
+
+                // % el resto dividido del numero dara 0  y 1 hasta que sea indivisible 
+                // se van haciendo sucesivas divisiones hasta no pueda dividirse  por 2 y sale del bucle con numero >=1 
+                // por no ser divisible 
+                // lo voy poniendo en una cadena los 0 y los 1 que sean del resto y concateneo según se opere
+                // y por ultimo se  invierte la cadena para ver su valor en binario                   
+
+                restoAlmacenado = numeroEntero % 2;
+               // restoAlmacenado = parseInt(restoAlmacenado);
+                //En esta variable guardamos el resto y se lo concatenamos al STRING ya almacenado.
+                cadenabinaria = "" + restoAlmacenado +"" +""+ cadenabinaria  + "" ;
+
+                numeroEntero /= 2;
+                numeroEntero = parseInt(numeroEntero);
+
+
+            } while (numeroEntero >= 1);
+
+
+    return cadenabinaria;
+
+}
+
+
+
+ /**
+ * 
+  * @type {devuelve la mantisa la parte que falten 0 se rellenan a la derecha hasta completar los 23 bits}    
+  *           */
+function vermantisa(svalorpasadobinario)
+{
+  var srelleno ="";
+  srelleno=svalorpasadobinario;
+  svalorpasadobinario= srelleno.padEnd(23,"0");  // completar con ceros a la derecha
+  return svalorpasadobinario;
+}
